d764aba54f02aadbe6087d194a416e057720e126
PRESSO <user1205@naver.com>
2022-03-01 01:59:44 +0900

    0.2.2

--- public/Main.js
+++ public/Main.js
@@ -1,6 +1,10 @@
-const {app, BrowserWindow, protocol} = require('electron');
+const {app, BrowserWindow, protocol, dialog} = require('electron');
 const path = require('path');
 const url = require('url');
+const isDev = require('electron-is-dev')
+
+const { autoUpdater } = require("electron-updater");
+const log = require('electron-log');
 
 function createWindow() {
   console.log(app.getPath('userData'));
@@ -40,10 +44,58 @@ function createWindow() {
 //   })
 // })
 
+/* Updater ======================================================*/
+
+autoUpdater.on('checking-for-update', (event) => {
+  log.info("Checking for update")
+})
+
+autoUpdater.on('update-available', (event) => {
+  log.info("Update Available : " + event.version)
+})
+
+autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
+    log.info("Update downloaded")
+    const dialogOpts = {
+        type: 'info',
+        buttons: ['재시작', '나중에'],
+        title: 'Application Update',
+        message: process.platform === 'win32' ? releaseNotes : releaseName,
+        detail: '새로운 버전이 다운로드 되었습니다. 업데이트를 적용하기 위해 앱을 재시작하세요.'
+    }
+    
+    dialog.showMessageBox(dialogOpts).then((returnValue) => {
+        log.info("returnValue: ", returnValue.response)
+        if (returnValue.response === 1) autoUpdater.quitAndInstall()
+    })
+})
+
+autoUpdater.on('download-progress', (progressObj) => {
+  log.info("Progress:" + progressObj.percent + '%')
+})
+
+autoUpdater.on('error', (message,error) => {
+    log.info("ERROR!!!!")
+    log.info(error)
+})
+
+/* Electron =====================================================*/
+
+
 app.on('ready', () => {
   // protocol.registerFileProtocol('atom', (request, callback) => {
   //   const pathname = decodeURIComponent(request.url.replace('file://', ''));
   //   callback({ path: path.normalize(pathname) })
   // });
   createWindow()
+
+  // 자동 업데이트 등록
+  if (!isDev) {
+    autoUpdater.checkForUpdates();
+  }
+});
+
+/** [생명주기] 모든 창이 닫히면 자동으로 앱 종료 */
+app.on('window-all-closed', () => {
+  app.quit();
 });
